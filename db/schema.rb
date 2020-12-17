c19dc9f8aaa3087217652da4862f78e234f5645b
Lucas <lucasdonuts@gmail.com>
2020-12-17 09:53:05 -0600

    Login issue fix

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_12_16_162825) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
